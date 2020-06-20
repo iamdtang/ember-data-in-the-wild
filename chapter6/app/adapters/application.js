7654ed057e27739d2abf4713be968cc096424152
David Tang <dtang@auditboard.com>
2020-06-19 17:22:17 -0700

    Removes Chapter 6 application in favor of https://github.com/skaterdav85/editw-chapter6. Please see latest book release

--- chapter6/app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import MyRESTAdapter from './my-rest';
-import LocalStorageAdapter from './local-storage';
-
-export default LocalStorageAdapter.extend({
-// export default MyRESTAdapter.extend({
-  namespace: 'api'
-});
--- chapter6/app/serializers/application.js
+++
@@ -1,4 +0,0 @@
-import MyRESTSerializer from './my-rest';
-
-export default MyRESTSerializer.extend({
-});
